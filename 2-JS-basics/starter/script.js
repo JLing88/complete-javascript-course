1c6dd53319164812162a1a1455e82f9c07863e71
Jesse Ling <jesse.ryan.ling@gmail.com>
2019-01-15 16:24:04 -0700

    First day

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,8 @@
+var now, yearJesseWasBorn, yearShaneWasBorn;
+
+now = 2018;
+yearJesseWasBorn = now - 35;
+yearShaneWasBorn = now - 39;
+console.log(yearJesseWasBorn);
+console.log(yearShaneWasBorn);
+console.log(now + 2);
